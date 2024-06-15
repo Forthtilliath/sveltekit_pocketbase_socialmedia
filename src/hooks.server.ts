10ca34964b5ed4a01f1058e6f1cb812bd409cc4c
Forthtilliath <forth@live.fr>
2024-06-15 18:25:52 +0200

    chore: add env variable for pb url

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,20 +1,28 @@
-import { PRIVATE_POCKETBASE_EMAIL, PRIVATE_POCKETBASE_PASSWORD } from '$env/static/private';
+import {
+	PRIVATE_POCKETBASE_URL,
+	PRIVATE_POCKETBASE_EMAIL,
+	PRIVATE_POCKETBASE_PASSWORD
+} from '$env/static/private';
 
 import { pb } from '$lib/pocketbase';
 import { fail } from '@sveltejs/kit';
 import PocketBase from 'pocketbase';
 
 export const handle = async ({ event, resolve }) => {
-	event.locals.pocketBase = new PocketBase('http://localhost:8090');
+	event.locals.pocketBase = new PocketBase(PRIVATE_POCKETBASE_URL);
+
+	const cookie = event.request.headers.get('cookie');
+	event.locals.pocketBase.authStore.loadFromCookie(cookie || '');
 
 	try {
-		event.locals.pocketBase.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
-		await event.locals.pocketBase.collection('users').authRefresh();
-	} catch (error) {
+		if (event.locals.pocketBase.authStore.isValid) {
+			await event.locals.pocketBase.collection('users').authRefresh();
+		}
+	} catch (_) {
 		event.locals.pocketBase.authStore.clear();
 	}
 
-	event.locals.pocketBaseAdmin = new PocketBase('http://localhost:8090');
+	event.locals.pocketBaseAdmin = new PocketBase(PRIVATE_POCKETBASE_URL);
 
 	try {
 		await event.locals.pocketBaseAdmin.admins.authWithPassword(
--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,11 +1,12 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 import PocketBase from 'pocketbase';
+import { PRIVATE_POCKETBASE_URL } from '$env/static/private';
 
 export const pb = writable<PocketBase | undefined>(undefined, (set) => {
 	if (!browser) return;
 
-	const pocketbaseInstance = new PocketBase('http://localhost:8090');
+	const pocketbaseInstance = new PocketBase(PRIVATE_POCKETBASE_URL);
 	pocketbaseInstance.authStore.loadFromCookie(document.cookie);
 
 	set(pocketbaseInstance);
